9a09f780bda34a3bf08b34eaa00b8c23c7b17be8
SimpleSecond <simplesecond@163.com>
2017-11-28 11:54:59 +0800

    Project初始上传

---
+++ frame-news/frame-news/Supportings/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  frame-news
+//
+//  Created by WangDongya on 2017/11/28.
+//  Copyright © 2017年 example. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
